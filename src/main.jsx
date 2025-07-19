21231543af915ea230251eb3abc46b3e5e04f05b
Cristhian <crijpardomer@unal.edu.co>
2025-07-19 08:41:54 -0500

    config. main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Amplify from 'aws-amplify'
+import awsExports from './aws-exports'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+Amplify.configure(awsExports)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
